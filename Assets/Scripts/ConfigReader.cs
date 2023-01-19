e72b6b99ff60a1b9865f883cd9a055a0c60eb17d
David Elias <delias200603@gmail.com>
2023-01-19 17:10:26 +0100

    started

--- Assets/Scripts/ConfigReader.cs
+++ Assets/Scripts/ConfigReader.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using 
 using UnityEngine;
 
 public class ConfigReader : MonoBehaviour
 {
     [SerializeField] TextAsset file;
 
+    
     private class TestClass
     {
         public int FirstField { get; set; }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,18 +6,44 @@ public class PlayerControl : MonoBehaviour
 {
 
     public Vector2 pos, speed, accel, maxSpeed;
+    public float gravity;
+    public bool isAir, isGround, isWall;
+    Collider2D coll;
     
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ReadDefaultValues();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /**
+     * Reads the values from the config file
+     * TODO: Change this to actually read the config file
+     */
+    private void ReadDefaultValues()
+    {
+        pos = transform.position;
+        speed = new Vector2(0, 0);
+        accel = new Vector2(0, 0);
+        maxSpeed = new Vector2(5, 5);
+        gravity = 9.81f;
+        isAir = false;
+        isGround = false;
+        isWall = false;
+
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void GravityCalculation()
+    {
+
     }
 
 }
